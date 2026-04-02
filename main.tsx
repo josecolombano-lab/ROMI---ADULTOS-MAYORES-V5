2a3ed84c8cc0e44974f9876bcee18d983b545604
josecolombano-lab <josecolombano@gmail.com>
2026-04-02 10:49:31 -0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ErrorBoundary } from './components/ErrorBoundary';
+import { ErrorBoundary } from './ErrorBoundary';
 
 console.log("ROMI: Iniciando aplicación...");
 try {
